f08fb42485eabf3c35ad17b472886ab7ede83b63
RA-WAI <rwaikyaw@gmail.com>
2024-04-10 13:59:08 +0700

    add admin lte wit vue

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\ApplicationController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('admin/dashboard', function(){
+//     return view('admin.dashboard');
+// });
+
+Route::get('{view}', ApplicationController::class)->where('view', '(.*)');
